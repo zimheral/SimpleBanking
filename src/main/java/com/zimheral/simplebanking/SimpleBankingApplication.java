4cc478d2ebbdb23c9e0faf07f80bba15469e0d31
zimheral <zimheral@gmail.com>
2020-11-11 14:24:17 +0100

    Added initial project structure, spring boot dependencies

---
+++ src/main/java/com/zimheral/simplebanking/SimpleBankingApplication.java
@@ -0,0 +1,12 @@
+package com.zimheral.simplebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zimheral/simplebanking/SimpleBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimheral.simplebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
